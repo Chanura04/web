78f9ba2deb39823f342e82b58f10890a411398b2
Chanura04 <167898990+Chanura04@users.noreply.github.com>
2025-03-13 16:01:31 +0530

    Add files via upload

---
+++ discount.js
@@ -0,0 +1,55 @@
+function con2(){
+    window.location.href="con2.html"
+}
+
+function con3(){
+    window.location.href="con3.html"
+}
+
+function con4(){
+    window.location.href="con4.html"
+}
+
+function con5(){
+    window.location.href="con5.html"
+}
+
+function con6(){
+    window.location.href="con6.html"
+}
+function con11(){
+    window.location.href="con11.html"
+}
+
+
+function news(){
+    window.location.href="news.html"
+}
+function DiscountMovies(){
+    window.location.href="DiscountMovies.html"
+}
+function DiscountConcerts(){
+    window.location.href="DiscountConcerts.html"
+}
+function signIn(){
+    window.location.href="signIn.html"
+}
+function profile(){
+    window.location.href="profile.html"
+}
+function aboutUS(){
+    window.location.href="aboutUS.html"
+}
+function contactUs(){
+    window.location.href="contactUs.html"
+}
+function feedback(){
+    window.location.href="feedback.html"
+}
+
+function home(){
+    window.location.href="index.html"
+}
+function pastEvent(){
+    window.location.href="pastEvent.html"
+}
---
+++ docMovi.js
@@ -0,0 +1,50 @@
+function film13(){
+    window.location.href="mad.html"
+}
+function film9(){
+    window.location.href="carol.html"
+}
+function film2(){
+    window.location.href="lilo.html"
+}
+function film1(){
+    window.location.href="mufasa.html"
+}
+function film6(){
+    window.location.href="close.html"
+}
+function film4(){
+    window.location.href="snow.html"
+}
+
+function news(){
+    window.location.href="news.html"
+}
+function DiscountMovies(){
+    window.location.href="DiscountMovies.html"
+}
+function DiscountConcerts(){
+    window.location.href="DiscountConcerts.html"
+}
+function signIn(){
+    window.location.href="signIn.html"
+}
+function profile(){
+    window.location.href="profile.html"
+}
+function aboutUS(){
+    window.location.href="aboutUS.html"
+}
+function contactUs(){
+    window.location.href="contactUs.html"
+}
+function feedback(){
+    window.location.href="feedback.html"
+}
+
+function home(){
+    window.location.href="index.html"
+}
+function pastEvent(){
+    window.location.href="pastEvent.html"
+}
