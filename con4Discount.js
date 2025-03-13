2f9a6b84434d2b5e66379c95700a4198150201ce
Chanura04 <167898990+Chanura04@users.noreply.github.com>
2025-03-13 21:50:13 +0530

    Add files via upload

---
+++ con4Discount.js
@@ -0,0 +1,36 @@
+
+function confTitle(Title){
+    localStorage.setItem('FilmName',Title);
+    window.location.href='consBookingDiscount.html';
+}
+function news(){
+    window.location.href="news.html"
+}
+function DiscountMovies(){
+    window.location.href="DiscountMovies.html"
+}
+function DiscountConcerts(){
+    window.location.href="DiscountConcerts.html"
+}
+function signIn(){
+    window.location.href="signIn.html"
+}
+function profile(){
+    window.location.href="profile.html"
+}
+function aboutUS(){
+    window.location.href="aboutUS.html"
+}
+function contactUs(){
+    window.location.href="contactUs.html"
+}
+function feedback(){
+    window.location.href="feedback.html"
+}
+
+function home(){
+    window.location.href="index.html"
+}
+function pastEvent(){
+    window.location.href="pastEvent.html"
+}
